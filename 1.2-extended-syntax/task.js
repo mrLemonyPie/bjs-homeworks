5939e066ce99f101c13fe470aabdf81d8db7aa53
mrLemonyPie <mr.lemon.lemon77@gmail.com>
2020-09-08 00:39:28 +0300

    Complete task: ex.1, ex.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,12 +1,43 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    
+    "use strict";
+
+    let b2 = Math.pow(b, 2);
+    let d = b2 - (4 * a * c);
+    let d2 = Math.sqrt(d);
+    let x = [];
+
+    if (d < 0) {
+        x;
+    } else if (d = 0) {
+        x = [(- b + d2) / (2 * a)];
+    } else {
+        x = [(- b + d2) / (2 * a), (- b - d2) / (2 * a)];
+    }
+    
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+    
+    let marksSum = 0;
+    let marksQuantity = marks.length;
+    let averageMark = "";
+            
+    if (marksQuantity == 0) {
+        averageMark = 0;
+    } else if (marksQuantity > 5) {
+        console.log("Учитываются только первые пять оценок!");
+        marks.slice(0, 5);
+    } else {
+        for (let i = 0; i < marksQuantity; i++) {
+            marksSum += marks[i];  
+        }      
+        averageMark = marksSum / marksQuantity;
+    }
+    
+    return averageMark;
+} 
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
