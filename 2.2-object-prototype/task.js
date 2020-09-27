f9b32c0dbe9a66e3b742f734d50d3415e986e7cc
mrLemonyPie <mr.lemon.lemon77@gmail.com>
2020-09-27 16:24:52 +0300

    Complete task 2.2-object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,28 @@
-//String.prototype.isPalindrome - для задачи №1
+// Задача №1
+    
+String.prototype.isPalindrome = function() {
+    
+    let normalStr = this.toLowerCase().match(/[а-я]/gi).reverse();
+   
+    return normalStr.join('') === normalStr.reverse().join('');
+}
+
+// Задача №2
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+    let marksSum = 0;
+    
+    for (let i = 0; i < marks.length; i++) {
+            marksSum += marks[i];  
+        }  
+        
+    let averageMark = marksSum / marks.length;
+
+    return Math.round(averageMark);
 }
 
-function checkBirthday(birthday) {
+/*function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
     // return verdict
-}
+}*/
